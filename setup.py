2a4e90c7ff4657012be4e9f6b946b5ca28632b85
NALS Quan Duc Binh <82556152+binhqd-nal@users.noreply.github.com>
2024-06-07 13:16:29 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.10',
     author='Hikaru ETO',
     author_email='kitadakyou@gmail.com',
-    url='https://kitadakyou.github.io/PyBacklogPy/',
+    url='https://github.com/binhqd-nal/PyBacklogPy.git',
     description='A library for backlog api',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
